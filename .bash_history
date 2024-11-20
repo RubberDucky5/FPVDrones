a31ff566ac4e9309c6ddd242f5aacad7d24a2a46
Glitch (glitch-hello-node) <none>
2024-11-20 15:50:44 -0500

    Init

---
+++ .bash_history
@@ -0,0 +1,16 @@
+ls
+rm -r ./
+ls
+rm -r *
+ls
+ls -a
+rm -ra *
+rm --help
+ls
+ls -a
+refresh
+npm init
+refresh
+node index.js
+npm install express
+npm start
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
-// const app = express();
-// const port = 8080;
+const app = express();
+const port = 8080;
 
-// app.get('/', (req, res ) => {
-//   res.sendFile(__dirname + "/index.html");
-// });
+app.use(express.static("public"));
 
-// app.listen(port, () => {
-//   console.log("Listening on port: " + port);
-// });
+app.get('/', (req, res ) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, () => {
+  console.log("Listening on port: " + port);
+});
